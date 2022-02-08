e83c2657cafc27448bc7f9ed3c06821ac2d0df1b
sumana2001 <sumana.basu2001@gmail.com>
2022-02-08 18:02:11 +0530

    Multiple Return Values

---
+++ multipleReturnValues/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func getInitials(n string) (string, string) {
+	s := strings.ToUpper(n)
+	names := strings.Split(s, " ")
+	var initials []string
+	for _, v := range names {
+		initials = append(initials, v[:1])
+	}
+	if len(initials) > 1 {
+		return initials[0], initials[1]
+	}
+	return initials[0], "_"
+}
+
+func main() {
+	first1, last1 := getInitials("john doe")
+	fmt.Println(first1, last1)
+	first2, last2 := getInitials("tina")
+	fmt.Println(first2, last2)
+}
