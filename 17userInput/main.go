907abb083162f7217a6660d8f6a0d8cad724d390
sumana2001 <sumana.basu2001@gmail.com>
2022-02-12 11:17:25 +0530

    User Input

---
+++ 17userInput/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, error := r.ReadString('\n')
+	return strings.TrimSpace(input), error
+}
+
+func createBill() bill {
+	reader := bufio.NewReader(os.Stdin)
+	name, _ := getInput("Create a new bill name: ", reader)
+	b := newBill(name)
+	fmt.Println("Created the bill - ", b.name)
+
+	return b
+}
+
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+	option, _ := getInput("Choose option (a - add item,s - save bill, t - add tip) ", reader)
+	fmt.Println(option)
+}
+
+func main() {
+	mybill := createBill()
+	promptOptions(mybill)
+}
